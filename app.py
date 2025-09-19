cad2e66e3bfe1796589a3456e9fac6c923f6d075
Anfal AR <kailanybayan@gmail.com>
2025-09-20 01:20:04 +0300

    Remove streamlit imports from Flask app

--- app.py
+++ app.py
@@ -508,7 +508,6 @@ def health():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
-import streamlit as st
 from study_plan_logic import generate_study_plan
 import pdfkit
 from jinja2 import Template
