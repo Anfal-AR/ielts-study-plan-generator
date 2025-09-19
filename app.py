fa579b86b33057dcce968b14aa9c21cd1e7d4ff4
Anfal AR <kailanybayan@gmail.com>
2025-09-20 01:52:29 +0300

    Remove unused pdfkit import

--- app.py
+++ app.py
@@ -509,7 +509,6 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 from study_plan_logic import generate_study_plan
-import pdfkit
 from jinja2 import Template
 import os
 import base64
